f56e3c7103a3e7cb7325aa85af6769684624eafb
zarsenkov <74777354+zarsenkov@users.noreply.github.com>
2026-02-10 22:02:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Подключаем наш файл стилей (проверь, чтобы имя файла в папке совпадало!)
+import './App.css'; 
+// Подключаем основной компонент игры
 import App from './App';
-import './index.css';
 
+// Создаем корень приложения и отрисовываем его на странице
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
